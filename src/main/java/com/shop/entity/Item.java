bb1297c6388bb444d0f742fb3fb3b92f38cdc449
youngseok.jang <ys123.jang@gmail.com>
2023-05-24 18:43:12 +0900

    Add item and repository

--- src/main/java/com/shop/entity/Item.java
+++ src/main/java/com/shop/entity/Item.java
@@ -1,4 +1,51 @@
 package com.shop.entity;
 
+import com.shop.constant.ItemSellStatus;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "item")
 public class Item {
+    @Id
+    @Column(name = "item_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String itemName;
+
+    @Column(name = "price", nullable = false)
+    private int price;
+
+    @Column(nullable = false)
+    private int stockNumber;
+
+    @Lob
+    @Column(nullable = false)
+    private String itemDetail;
+
+    @Enumerated(EnumType.STRING)
+    private ItemSellStatus itemSellStatus;
+    private LocalDateTime regTime;
+    private LocalDateTime updateTime;
+
+    @Builder
+    public Item(String itemName, int price, int stockNumber, String itemDetail, ItemSellStatus itemSellStatus, LocalDateTime regTime, LocalDateTime updateTime) {
+        this.itemName = itemName;
+        this.price = price;
+        this.stockNumber = stockNumber;
+        this.itemDetail = itemDetail;
+        this.itemSellStatus = itemSellStatus;
+        this.regTime = regTime;
+        this.updateTime = updateTime;
+    }
 }
---
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -0,0 +1,45 @@
+package com.shop.repository;
+
+import com.shop.constant.ItemSellStatus;
+import com.shop.entity.Item;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
+class ItemRepositoryTest {
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Test
+    @DisplayName("상품 저장 테스트")
+    public void createItemTest() {
+        // given
+        Item item = Item.builder()
+                .itemName("test")
+                .price(10000)
+                .itemDetail("test item detail")
+                .itemSellStatus(ItemSellStatus.SELL)
+                .stockNumber(100)
+                .regTime(LocalDateTime.now())
+                .updateTime(LocalDateTime.now())
+                .build();
+
+        // when
+        Item savedItem = itemRepository.save(item);
+
+        // then
+        assertThat(savedItem).isEqualTo(item);
+
+    }
+
+}
